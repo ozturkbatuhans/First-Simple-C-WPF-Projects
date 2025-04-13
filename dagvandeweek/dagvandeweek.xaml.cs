de2fc87709cdc27f297f255999eeb935fdc069bc
ozturkbatuhann <r1036481@student.vives.be>
2025-04-13 23:12:55 +0200

    add the project

---
+++ dagvandeweek/dagvandeweek.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dagvandeweek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int iDagNummer = int.Parse(txtDagNummer.Text);
+
+            lblResult.Content = ((iDagNummer == 1) ? "Maandag" :
+                                 (iDagNummer == 2) ? "Dinsdag" :
+                                  (iDagNummer == 3) ? "Woensdag" :
+                                   (iDagNummer == 4) ? "Donderdag" :
+                                    (iDagNummer == 5) ? "Vrijdag" :
+                                     (iDagNummer == 6) ? "Zaterdag" :
+                                      (iDagNummer == 7) ? "Zondag" : "ongeldig input");
+        }
+    }
+}
---
+++ temperatuur/temperatuur.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace temperatuur
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+           
+            int temperatuur = int.Parse(txttemperatuur.Text);
+
+            txtResult.Text = (temperatuur > 25) ? "Het is warm" : "Het is koel";
+        }
+    }
+}
